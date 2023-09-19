4ba7fdd66e2a93a12cd464ff1e509388d0bbe9b3
Andre Grandon <andre@andresmacstudio.home>
2023-09-19 15:32:41 -0400

    tester

--- breddit_django/settings.py
+++ breddit_django/settings.py
@@ -26,9 +26,10 @@ SECRET_KEY = 'DJANGO_SECRET_KEY'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+
 
 AUTH_USER_MODEL = 'bit_users.User'
 
