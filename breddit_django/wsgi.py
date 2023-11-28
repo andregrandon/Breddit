600201b2023a2abdf1425be1822fbbdba0c4b8fb
Andre Grandon <andre@AndresMacStudio.lan>
2023-11-28 16:40:24 -0700

    typo2

--- breddit_django/wsgi.py
+++ breddit_django/wsgi.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
-from breddit_django.wsgi import application
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "breddit_django.settings")
